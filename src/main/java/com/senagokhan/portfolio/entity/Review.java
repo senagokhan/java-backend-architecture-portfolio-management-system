1126ff4787974be2699caaf397810b4ec2708589
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2025-05-04 18:00:35 +0300

    Review and User Entity Added

---
+++ src/main/java/com/senagokhan/portfolio/entity/Review.java
@@ -0,0 +1,37 @@
+package com.senagokhan.portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
