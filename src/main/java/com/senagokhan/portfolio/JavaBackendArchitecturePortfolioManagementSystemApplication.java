3fa2d43b41df4837f34fcc9fe37cc9a048f048f1
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2025-05-04 17:25:28 +0300

    Fix: Push Spring project from correct root directory

---
+++ src/main/java/com/senagokhan/portfolio/JavaBackendArchitecturePortfolioManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.senagokhan.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendArchitecturePortfolioManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendArchitecturePortfolioManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senagokhan/portfolio/JavaBackendArchitecturePortfolioManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senagokhan.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendArchitecturePortfolioManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
